e124f02728cb07212bb680307bd537164d033303
Christopher Harwell <chrisharwell72@gmail.com>
2021-01-18 15:37:51 -0500

    removes boilerplate code

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, TextInput, View } from 'react-native';
 
 export default function App() {
-
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>    
-      <Button title="Change Text" onPress={() => setOutputText('The text changed')} />
+    <View style={{padding: 30}}>
+      <View>
+        <TextInput/>
+        <Button title="ADD"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
